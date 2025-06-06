0277db0e83b1c6b3d0eaedaa4ce0e40eca04f2ee
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-06-06 10:42:51 -0600

    se crea usuario administrador por defecto

---
+++ configs/setUpData.js
@@ -0,0 +1,28 @@
+import User from '../src/user/user.model.js'
+import { encrypt } from '../utils/encrypt.js'
+
+
+export const createDefaultAdmin = async () => {
+    try {
+        const adminEmail = "admin@default.com"
+        const existingAdmin = await User.findOne({ email: adminEmail })
+
+        if (!existingAdmin) {
+            const hashedPassword = await encrypt("123.sd/")
+            const adminUser = new User({
+                name: "Admin",
+                surname: "Default",
+                email: adminEmail,
+                DPI: 1234567891234,
+                password: hashedPassword,
+                phone: "123456789",
+                role: "ADMIN",
+                status: true
+            })
+
+            await adminUser.save()
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
