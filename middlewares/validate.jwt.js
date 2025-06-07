e581bba766538ec3567dd6ffa2d6f592491a2abe
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-06-07 16:03:25 -0600

    se corrije error de validacion de token

--- middlewares/validate.jwt.js
+++ middlewares/validate.jwt.js
@@ -1,8 +1,8 @@
 'use strict '
 import jwt from 'jsonwebtoken'
 
-import { findUser } from '../helpers/db.validators.js'
-
+import { findUser } from '../helpers/db.validator.js'
+/*
 export const validateJwt = async(req, res, next) =>{
     try {
         let secretKey = process.env.SECRET_KEY
@@ -16,6 +16,33 @@ export const validateJwt = async(req, res, next) =>{
         } catch (err) {
         console.error(err)
         return res.status(401).send({success: false, message: 'Invalid Token'})
+    }
+}*/
+
+export const validateJwt = async(req, res, next)=>{
+    try{
+        let secretKey = process.env.SECRET_KEY
+        let { authorization } = req.headers
+        if(!authorization) return res.status(401).send({message: 'Unauthorized'})
+
+
+        let user = jwt.verify(authorization, secretKey)
+        const validateUser = await findUser(user.uid)
+        if(!validateUser) return res.status(404).send(
+            {
+                success: false,
+                message: 'User not found - Unauthorized'
+            }
+        )
+        req.user = {
+            id: validateUser._id,
+            email: validateUser.email,
+            role: validateUser.role
+        }
+        next()
+    }catch(err){
+        console.error(err)
+        return res.status(401).send({message: 'Invalid token or expired'})
     }
 }
 
