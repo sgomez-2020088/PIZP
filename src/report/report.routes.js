9780e063ecf4de60b17c9f04efa1516965276f68
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-06-07 16:07:20 -0600

    se actualizan rutas con validacion

--- src/report/report.routes.js
+++ src/report/report.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { addReport, getReports, deleteReport } from "./report.controller.js"
 import { isAdmin, validateJwt } from "../../middlewares/validate.jwt.js"
+import { reportValidator } from "../../helpers/validators.js"
 
 const api = Router()
 
-api.post('/add',[validateJwt], addReport)
+api.post('/add',[validateJwt, reportValidator], addReport)
 api.get('/get',[validateJwt], getReports)
 api.delete('/delete/:reportId',[validateJwt, isAdmin], deleteReport)
 
