11f3c552380a3cae1f5a8fdb0d811c0d613cfeb3
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-07-17 08:51:05 -0600

    fix: se soluciona error de merge

--- configs/mongo.js
+++ configs/mongo.js
@@ -25,7 +25,7 @@ export const connect = async()=>{
 
         await  mongoose.connect(
 
-            //`${process.env.DB_SERVICE}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+            //`${process.env.DB_SERVICE}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,a
             process.env.URI_MONGO,
             {
                 maxPoolSize: 50, 
